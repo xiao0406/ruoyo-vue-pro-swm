46f0959d4d39844987565918c5e8fb0016c660b6
xy <13140175177@163.com>
2025-11-11 14:50:47 +0800

    修改看板bug

--- swm/swm/src/main/java/com/jeesite/modules/swm/service/SwmDashboardService.java
+++ swm/swm/src/main/java/com/jeesite/modules/swm/service/SwmDashboardService.java
@@ -248,7 +248,7 @@ public class SwmDashboardService {
 //            String todayEndTime = sdf.format(calendar.getTime());
 
             Date now = new Date();
-            Date oneMinuteAgo = DateUtil.offsetMinute(now, -600);
+            Date oneMinuteAgo = DateUtil.offsetMinute(now, -5);
             String todayStartTime = DateUtil.formatDateTime(oneMinuteAgo);
             String todayEndTime = DateUtil.formatDateTime(now);
             
