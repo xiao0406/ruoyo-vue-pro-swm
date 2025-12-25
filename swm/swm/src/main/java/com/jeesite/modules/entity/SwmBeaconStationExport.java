1c6115efba419ff0f5204eadaf7bff86c0420069
xy <13140175177@163.com>
2025-12-25 11:17:58 +0800

    信标导入逻辑优化

--- swm/swm/src/main/java/com/jeesite/modules/entity/SwmBeaconStationExport.java
+++ swm/swm/src/main/java/com/jeesite/modules/entity/SwmBeaconStationExport.java
@@ -9,8 +9,14 @@ public class SwmBeaconStationExport {
 
 
     @ExcelFields({
-            @ExcelField(title = "MAC地址", attrName = "beaconId", align = ExcelField.Align.CENTER, sort = 10,width = 256*30),
-            @ExcelField(title = "所属区域", attrName = "area", align = ExcelField.Align.CENTER, sort = 20,width = 256*30),
+            @ExcelField(title = "MAC地址", attrName = "beaconId", align = ExcelField.Align.CENTER, sort = 10,width = 256*20),
+            @ExcelField(title = "设备名称", attrName = "deviceName", align = ExcelField.Align.CENTER, sort = 20,width = 256*20),
+            @ExcelField(title = "所在位置", attrName = "location", align = ExcelField.Align.CENTER, sort = 10,width = 256*20),
+            @ExcelField(title = "所属区域", attrName = "area", align = ExcelField.Align.CENTER, sort = 20,width = 256*20),
+            @ExcelField(title = "图纸像素X坐标", attrName = "pixelX", align = ExcelField.Align.CENTER, sort = 20,width = 256*20),
+            @ExcelField(title = "图纸像素Y坐标", attrName = "pixelY", align = ExcelField.Align.CENTER, sort = 20,width = 256*20),
+            @ExcelField(title = "信标类型", attrName = "beaconType", align = ExcelField.Align.CENTER, sort = 20,width = 256*10,dictType = "beacon_type_enum"),
+            @ExcelField(title = "信标状态（在线、离线）", attrName = "beaconStatus", align = ExcelField.Align.CENTER, sort = 20,width = 256*30,dictType = "beacon_status_enum"),
     })
 
     public SwmBeaconStationExport() {
@@ -18,7 +24,13 @@ public class SwmBeaconStationExport {
     }
 
     private String beaconId; // MAC地址
+    private String deviceName; // 设备名称
     private String area; // MAC地址
+    private Double pixelX; // 图纸像素X坐标
+    private Double pixelY; // 图纸像素Y坐标
+    private String location; // 所在位置
+    private String beaconType; // 信标类型
+    private String beaconStatus; // 信标状态
 
 
 }
