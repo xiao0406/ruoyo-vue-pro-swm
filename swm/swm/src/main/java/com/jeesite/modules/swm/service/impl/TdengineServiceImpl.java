bb03bff8e5470dfb3e15ac8f953e125bdcd4badd
xy <13140175177@163.com>
2026-01-12 16:56:23 +0800

    排查定时任务 日考勤统计

--- swm/swm/src/main/java/com/jeesite/modules/swm/service/impl/TdengineServiceImpl.java
+++ swm/swm/src/main/java/com/jeesite/modules/swm/service/impl/TdengineServiceImpl.java
@@ -878,7 +878,7 @@ public class TdengineServiceImpl implements TDengineService {
         // 3. 替换 SQL 中的占位 {db} 为真实数据库名
         String realSql = sql.replace(dbname, dbNameNew);
 
-        XxlJobHelper.log("查询SQL: {}", sql);
+        XxlJobHelper.log("定时任务查询SQL: {}", sql);
 
         try {
 
